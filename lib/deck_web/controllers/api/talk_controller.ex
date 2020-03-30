2abfaacbdc44def0ce441c2d480be526134b2a24
Will <will.ockelmann.wagner@gmail.com>
2020-03-30 00:25:29 -0700

    add the edit flag to talks

--- lib/deck_web/controllers/api/talk_controller.ex
+++ lib/deck_web/controllers/api/talk_controller.ex
@@ -2,6 +2,7 @@ defmodule DeckWeb.Api.TalkController do
   use DeckWeb, :controller
 
   alias Deck.Talks
+  alias Deck.Auth
 
   action_fallback DeckWeb.FallbackController
 
@@ -11,7 +12,9 @@ defmodule DeckWeb.Api.TalkController do
   end
 
   def show(conn, %{"id" => slug}) do
+    user = Auth.current_user(conn)
+
     talk = Talks.get_talk_by_slug!(slug)
-    render(conn, "show.json", talk: talk)
+    render(conn, "show.json", talk: talk, user: user)
   end
 end
--- lib/deck_web/views/api/talk_view.ex
+++ lib/deck_web/views/api/talk_view.ex
@@ -8,11 +8,13 @@ defmodule DeckWeb.Api.TalkView do
     %{data: render_many(talk, TalkView, "talk.json")}
   end
 
-  def render("show.json", %{talk: talk}) do
-    %{data: render_one(talk, TalkView, "talk.json")}
+  def render("show.json", %{talk: talk} = params) do
+    %{data: render_one(talk, TalkView, "talk.json", params)}
   end
 
-  def render("talk.json", %{talk: talk}) do
+  def render("talk.json", %{talk: talk} = params) do
+    can_edit = params[:user] && params[:user].id == talk.user_id
+
     %{
       id: talk.id,
       name: talk.name,
@@ -20,7 +22,8 @@ defmodule DeckWeb.Api.TalkView do
       audio: audio_url(talk),
       motion_capture: motion_capture_url(talk),
       theme: talk.theme,
-      deck: talk.deck
+      deck: talk.deck,
+      edit: can_edit
     }
   end
 
