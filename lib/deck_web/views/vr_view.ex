60e577c5faab7c834c39d9b2999bf62d055932ff
Will Ockelmann-Wagner <will@ae.studio>
2020-04-11 00:27:43 -0700

    pre-render slides for performance

--- lib/deck_web/views/vr_view.ex
+++ lib/deck_web/views/vr_view.ex
@@ -33,4 +33,71 @@ defmodule DeckWeb.VrView do
       deck: talk.deck
     }
   end
+
+  def render_slides(talk) do
+    theme = talk.theme
+
+    talk.deck["slides"]
+    |> Enum.with_index()
+    |> Enum.map(fn {slide, i} ->
+      render_slide(slide, i, theme)
+    end)
+  end
+
+  def render_slide(slide, i, theme) do
+    {elements, _} =
+      slide
+      |> Enum.reduce({[], 0}, fn slide, {elements, y} ->
+        {element, y} = render_slide_line(slide, y, theme)
+
+        {[element | elements], y}
+      end)
+
+    lines = Enum.reverse(elements)
+
+    content_tag("a-entity", lines,
+      id: "slide-#{i}",
+      mixin: "slide",
+      slide: to_attribute(slide: i)
+    )
+  end
+
+  def render_slide_line(%{"kind" => "img", "image" => image}, y, _) do
+    element =
+      content_tag("a-image", "",
+        src: "#image-#{image}",
+        position: "2.5 #{y} 0",
+        height: 5,
+        width: 5
+      )
+
+    {element, y - 5}
+  end
+
+  def render_slide_line(%{"kind" => kind, "content" => content}, y, %{"styles" => styles}) do
+    theme_styles = styles[kind] || styles["p"]
+    font_size = theme_styles["fontSize"]
+
+    element =
+      content_tag(
+        "a-entity",
+        "",
+        mixin: "slide__text",
+        position: "0 #{y} 0",
+        "text-geometry":
+          [value: content, font: "#optimerBoldFont", size: font_size]
+          |> to_attribute()
+      )
+
+    {element, y - font_size}
+  end
+
+  def to_attribute(keyword) do
+    keyword
+    |> Enum.reduce([], fn {key, value}, attributes ->
+      ["#{key}: #{value}" | attributes]
+    end)
+    |> Enum.reverse()
+    |> Enum.join("; ")
+  end
 end
