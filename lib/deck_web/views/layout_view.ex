2222f7c871fd1fa062b4f580050f92f305fb9e66
Will Ockelmann-Wagner <will.ockelmann.wagner@gmail.com>
2020-04-25 17:02:50 -0700

    add social meta data

--- lib/deck_web/views/layout_view.ex
+++ lib/deck_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule DeckWeb.LayoutView do
   use DeckWeb, :view
+
+  alias DeckWeb.Endpoint
+  alias DeckWeb.Router.Helpers, as: Routes
+
+  def social_image(conn) do
+    Endpoint.static_url() <> Routes.static_path(conn, "/images/social.png")
+  end
 end
